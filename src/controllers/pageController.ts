f71797665c69d2f83714b99b573bdc520dd4b09c
Ronaldo Pereira Da Silva <silva.ronaldosilva.ronaldo7@gmail.com>
2024-08-19 00:34:23 -0300

    testando as controllers

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,10 +1,11 @@
 import { Request, Response} from "express";
 
 export const home = (req:Request, res: Response) => {
-     res.send('home no controller'); //testando o controller
+     res.send('testando a home no controller '); //testando o controller
      //res.render ('pages/ Page'); 
 }
 
 export const fotos = (req:Request, res: Response) => {
+     res.send('paginas fotos  testando'); 
      //res.render ('pages/ Page'); 
 }
